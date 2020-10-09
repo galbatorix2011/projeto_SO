54fd831f8cea7b427197685554207eb8bf42204e
galbatorix2011 <afonsoalmeida.b@gmail.com>
2020-10-09 09:37:46 +0100

    prototipo de pool de threads adicionado

---
+++ so-20-21-ex1_base/fs/thread_pool.c
@@ -0,0 +1,22 @@
+#include "thread_pool.h"
+#include <stdlib.h>
+#include <pthread.h>
+
+thread_pool init_thread_pool(int size){
+    thread_pool t_pool;
+    t_pool.index = 0;
+    t_pool.size = size;
+    t_pool.pool = malloc(sizeof(pthread_t) * size);
+    return t_pool;
+}
+
+pthread_t get_pthread(thread_pool *t_pool){
+    pthread_join(t_pool->pool[t_pool->index], NULL);
+    pthread_t thread = t_pool->pool[t_pool->index];
+    t_pool->index = t_pool->index = (t_pool->size - 1) ? 0 : t_pool->index++; /* cicle throught the array */
+    return thread;
+}
+
+void clean_thread_pool(thread_pool *t_pool){
+    free(t_pool->pool);
+}
